7d8f31cc9b5e03a61346283ebc62c6af2d59a2ae
맥동우 <t6u80o@naver.com>
2025-01-01 21:28:48 +0900

    feat : 로그인, 회원가입 페이지 라우팅

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,20 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "HOME",
+  title: "홈",
 };
 
 export default function HomePage() {
-  return <div>HOME123</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen"> 
+      <Link className="hover:underline text-blue-900" href="/auth/login">
+        로그인
+      </Link>
+      <br />
+      <Link className="hover:underline text-blue-900" href="/auth/join-us">
+        회원가입
+      </Link>
+    </div>
+  );
 }
---
+++ app/(sign)/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "로그인",
+};
+
+export default function SignInPage() {
+  return <div>SIGN-IN</div>;
+}
