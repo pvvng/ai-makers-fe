d497b6610bf9bb4663eedca9c1cf5e10e614cee1
맥동우 <t6u80o@naver.com>
2025-01-05 22:50:38 +0900

    주석 제거

--- app/join-us/components/Body.tsx
+++ app/join-us/components/Body.tsx
@@ -8,7 +8,6 @@ import PurposeInterestContestSelectBox from "./bodyItems/PurposeInterestContestS
 import AvailabilityLocation from "./bodyItems/Location";
 
 export default function JoinUsBody() {
-
   return (
     <form className="bg-white p-5 sm:p-10 mt-5 border">
       <p className="text-lg">회원가입하고 다양한 해택을 누리세요!</p>
@@ -23,10 +22,3 @@ export default function JoinUsBody() {
     </form>
   );
 }
-
-// 서비스 가입 목적 - 1. 공모전 2. 스터디
-// 관심주제 - 로봇/게임/ai/어플 등등
-// 사용 가능 언어 - c++/java/python 등등
-// 공모전 나간 횟수 - 0/1/2/3/4/5회 이상
-// 활동가능요일 - 월/화/수/목/금/토/일
-// 거주지
