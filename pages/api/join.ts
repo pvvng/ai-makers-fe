7f555a78e34b78bd16d20c1acc340754a7ca90aa
맥동우 <t6u80o@naver.com>
2025-01-09 11:31:00 +0900

    feat : alert message 보이게, 다시 redirect 되도록

--- pages/api/join.ts
+++ pages/api/join.ts
@@ -4,6 +4,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  console.log(req.body);
   // 처리 로직
-  return res.status(200).json(req.body);
+  return res.status(200).json({ message: "회원가입이 완료되었습니다." });
 }
