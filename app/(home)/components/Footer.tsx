b6e67a2728449d6f60e27109afaed263ec5367ab
맥동우 <t6u80o@naver.com>
2025-01-13 23:05:24 +0900

    design : footer 기본형으로 수정

--- app/(home)/components/Footer.tsx
+++ app/(home)/components/Footer.tsx
@@ -1,17 +1,9 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Footer() {
   return (
     <div className="w-full p-4 pt-10 pb-10 px-8">
-      <h3 className="text-3xl font-black">
-        <Image
-          src="/banner/rocket-login.webp"
-          alt="로고"
-          width={100}
-          height={100}
-        />
-      </h3>
+      <h3 className="text-3xl font-black">LOGO</h3>
       <Link
         href="https://kr.freepik.com"
         target="_blank"
