b9bd7da5c634d210fd13815f7c2875a32dbfaac8
맥동우 <t6u80o@naver.com>
2025-01-15 03:07:13 +0900

    refactor : 변경한 함수로 수정

--- util/function/validation/validateFormValues.ts
+++ util/function/validation/validateFormValues.ts
@@ -1,15 +1,10 @@
-// type
-import { FormFieldName, FormValues } from "@/types/form";
 // validation functions
-import {
-  validateBirth,
-  validateId,
-  validateLanguageDayLocationValue,
-  validateNickname,
-  validatePassword,
-  validatePasswordConfirmInForm,
-  validateSelectValue,
-} from "./joinUsForm";
+import validateId from "./joinUs/id";
+import validatePassword from "./joinUs/password";
+import validateNickname from "./joinUs/nickname";
+import validateBirth from "./joinUs/birth";
+import validateSelectValue from "./joinUs/selectedValue";
+import validateLanguageDayLocationValue from "./joinUs/languageDayLocationValue";
 // constants
 import {
   AVAILABLE_DAY_LABEL,
@@ -20,11 +15,15 @@ import {
   INTEREST_LABEL,
   PURPOSE_LABEL,
 } from "@/constants/validation/selectValueLabel";
+// type
+import { FormFieldName, FormValues } from "@/types/form";
+import { ValidationResultType } from "@/types/joinUs";
 
 export function validateFormValues(formValues: FormValues) {
   const formObject = createFormObject(formValues);
 
-  const validationResults = getValidationResults(formObject);
+  const validationResults: ValidationResultType =
+    getValidationResults(formObject);
 
   return loopValidationResults(validationResults);
 }
@@ -37,40 +36,33 @@ function createFormObject(formValues: FormValues) {
 }
 
 function getValidationResults(formObject: Record<FormFieldName, string>) {
+  const { id, password, confirmPassword } = formObject;
+  const { nickname, birth } = formObject;
+  const { gender, purpose, interest, contest } = formObject;
+  const { language, day, location } = formObject;
+
   return [
-    validateId(formObject.id),
-    validatePassword(formObject.password),
-    validatePasswordConfirmInForm(
-      formObject.password,
-      formObject.confirmPassword
-    ),
-    validateNickname(formObject.nickname),
-    validateBirth(formObject.birth),
-
-    validateSelectValue(formObject.gender, GENDER_LABEL),
-    validateSelectValue(formObject.purpose, PURPOSE_LABEL),
-    validateSelectValue(formObject.interest, INTEREST_LABEL),
-    validateSelectValue(formObject.contest, CONTEST_LABEL),
-
-    validateLanguageDayLocationValue(
-      formObject.language,
-      AVAILABLE_LANGUAGE_LABEL
-    ),
-    validateLanguageDayLocationValue(formObject.day, AVAILABLE_DAY_LABEL),
-    validateLanguageDayLocationValue(
-      formObject.location,
-      AVAILABLE_LOCATION_LABEL
-    ),
+    validateId(id),
+    validatePassword(password, confirmPassword),
+    validateNickname(nickname),
+    validateBirth(birth),
+    validateSelectValue(gender, GENDER_LABEL),
+    validateSelectValue(purpose, PURPOSE_LABEL),
+    validateSelectValue(interest, INTEREST_LABEL),
+    validateSelectValue(contest, CONTEST_LABEL),
+    validateLanguageDayLocationValue(language, AVAILABLE_LANGUAGE_LABEL),
+    validateLanguageDayLocationValue(day, AVAILABLE_DAY_LABEL),
+    validateLanguageDayLocationValue(location, AVAILABLE_LOCATION_LABEL),
   ];
 }
 
-export function loopValidationResults(validationResults: (string | null)[]) {
+export function loopValidationResults(validationResults: ValidationResultType) {
   let validation = true;
 
   for (const result of validationResults) {
-    if (result) {
+    if (!result.ok) {
       validation = false;
-      alert(result);
+      alert(result.message);
       break;
     }
   }
