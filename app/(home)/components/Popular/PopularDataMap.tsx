378e18b8b952d5f86053b03007df47af59ae4dd4
맥동우 <t6u80o@naver.com>
2025-01-14 19:25:09 +0900

    refactor : 에러 컴포넌트 점검. 디자인 맞추기

--- app/(home)/components/Popular/PopularDataMap.tsx
+++ app/(home)/components/Popular/PopularDataMap.tsx
@@ -2,24 +2,33 @@
 import { CarouselData } from "@/types/carousel";
 // components
 import PopularCard from "./PopularCard";
+// icon
 import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function PopularDataMap({
-  popularData,
-  name,
-  icon,
-}: {
+interface PropsType {
   popularData: CarouselData[] | undefined;
   name: string;
   icon: IconDefinition;
-}) {
+}
+
+export default function PopularDataMap({ popularData, name, icon }: PropsType) {
   return (
     <div className="w-1/4 min-h-[450px] p-4 border rounded-2xl shadow bg-white flex items-center">
-      <div className="w-full">
-        <HeadTitle icon={icon} name={name} />
-        <Content popularData={popularData} />
-      </div>
+      {popularData ? (
+        <DataMap icon={icon} name={name} popularData={popularData} />
+      ) : (
+        <p className="text-center">{name} 데이터를 불러오는 중 에러가 발생했습니다.</p>
+      )}
+    </div>
+  );
+}
+
+function DataMap({ icon, name, popularData }: PropsType) {
+  return (
+    <div className="w-full">
+      <HeadTitle icon={icon} name={name} />
+      <Content popularData={popularData} />
     </div>
   );
 }
