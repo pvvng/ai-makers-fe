ea6f981b3ba8122314b0f9e1937c049db1f4f5ac
맥동우 <t6u80o@naver.com>
2025-01-19 16:21:42 +0900

    refactor : popular 컴포넌트 리팩토링

--- app/(home)/components/Popular/PopularStudy.tsx
+++ app/(home)/components/Popular/PopularStudy.tsx
@@ -9,20 +9,16 @@ import { APP_URL } from "@/constants/url";
 import PopularErrorContainer from "./PopularErrorContainer";
 
 export default async function PopularStudy() {
-  const popularContestResponse = await fetch(`${APP_URL}/api/popular-studies`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
+  const popularContestResponse = await fetch(`${APP_URL}/api/popular/studies`, {
     cache: "force-cache",
   });
+
+  const popularContestData: CarouselData[] =
+    await popularContestResponse.json();
 
   if (!popularContestResponse.ok) {
     return <PopularErrorContainer name="스터디" />;
   }
-
-  const popularContestData: CarouselData[] =
-    await popularContestResponse.json();
 
   return (
     <PopularDataMap
